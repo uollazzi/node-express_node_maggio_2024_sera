c3028c9d74da2902c74ae7c380496f43ccbf5385
Matteo Valenzi <matteo.valenzi@outlook.com>
2024-05-15 21:13:49 +0200

    file statici

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,27 @@
-console.log("Ciao!");
+import express, { NextFunction, Request, Response } from "express";
+import { DateTime } from "luxon";
+import path from "node:path";
+
+const app = express();
+const port = 3000;
+
+// middlewares
+app.use((req: Request, res: Response, next: NextFunction) => {
+    console.log("LOG:", req.method, req.url, DateTime.local().toFormat("dd/MM/yyyy HH:mm:ss"));
+    next();
+});
+
+app.use(express.static("./public")); // middelware file statici
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Ciao");
+});
+
+// app.get("/eiffel.jpg", (req: Request, res: Response) => {
+//     res.sendFile(path.join(process.cwd(), "eiffel.jpg"));
+// });
+
+
+app.listen(port, () => {
+    console.log(`Server in esecuzione su http://localhost:${port}`);
+});
