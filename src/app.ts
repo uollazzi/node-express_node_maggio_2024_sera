9a143859f1edcffe115460daca7bd4c5114fe920
Matteo Valenzi <matteo.valenzi@outlook.com>
2024-05-16 20:14:33 +0200

    .

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ app.use("/api", (req: Request, res: Response) => {
 });
 
 app.get("/", (req: Request, res: Response) => {
-    res.render("index");
+    res.render("index", { pageTitle: "Home Page" });
 });
 
 app.get("/errore", (req: Request, res: Response) => {
