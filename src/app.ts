3210775f68ac032b48ad71f3975836d7ca04126d
Matteo Valenzi <matteo.valenzi@outlook.com>
2024-05-16 21:11:38 +0200

    .

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 import { DateTime } from "luxon";
 import path from "node:path";
+import articoli from "./data";
 
 const app = express();
 const port = 3000;
@@ -31,6 +32,40 @@ app.use("/api", (req: Request, res: Response) => {
 app.get("/", (req: Request, res: Response) => {
     res.render("index", { pageTitle: "Home Page" });
 });
+
+app.get("/chi-siamo", (req: Request, res: Response) => {
+    res.render("chi-siamo", { pageTitle: "Chi Siamo" });
+});
+
+app.get("/contatti", (req: Request, res: Response) => {
+    res.render("contatti", { pageTitle: "Contatti" });
+});
+
+app.get("/articoli", (req: Request, res: Response) => {
+    res.render("articoli", { pageTitle: "Articoli", articoli });
+});
+
+app.get("/articolo/:id", (req: Request, res: Response) => {
+    const id = req.params["id"];
+    const idNumber = Number(id);
+
+    if (isNaN(idNumber)) {
+        // render 404
+        res.status(400).send("Errore nella conversione dell'id in numero.");
+        return;
+    }
+
+    // simulando il recupero dati da mongodb
+    const articolo = articoli.find(x => x.id == idNumber);
+
+    if (!articolo) {
+        res.status(404).send("Articolo non trovato.");
+        return;
+    }
+
+    res.render("articolo", { pageTitle: "Dettaglio Articolo", articolo });
+});
+
 
 app.get("/errore", (req: Request, res: Response) => {
     throw new Error("Database irraggiungibile");
